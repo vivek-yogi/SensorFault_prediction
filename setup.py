8fd553a053b7a055164f775477f14e372d4a9981
vivek-yogi <101085320+vivek-yogi@users.noreply.github.com>
2024-06-02 18:52:03 +0530

    Setup file added,sensor package created

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->list[str]:
+
+    requirements_list = list[str] = []
+    return requirements_list
+
+setup (
+    name         = 'SensorFault_prediction',
+    version      = '0.0.1',
+    author       = 'Vivek_yogi',
+    author_email = 'vivek87yogi@gmail.com',
+    packages     = find_packages(),
+    install_requires = get_requirements() ,#['pymongo']
+)
